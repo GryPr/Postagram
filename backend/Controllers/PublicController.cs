e6d9a888ff78ac92dfd5b89de9b2647e029d0f38
Kevin <promkev@gmail.com>
2021-02-09 14:35:37 -0500

    Add blank API w/ auth and w/o auth (#13)
    Add placeholder responses for API (#13)
    
    Update the README.md with new info (#13)
    
    Fix API auth (#13)

--- backend/Controllers/PublicController.cs
+++ backend/Controllers/PublicController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,18 +9,18 @@ using Microsoft.Identity.Web.Resource;
 
 namespace ImageStoreApi.Controllers
 {
-    [Authorize]
+
+    // Manages the public API (it doesn't require authentication and authorization)
     [ApiController]
     [Route("[controller]")]
-    public class ImageController : ControllerBase
+    public class PublicController : ControllerBase
     {
-
-        private readonly ILogger<ImageController> _logger;
+        private readonly ILogger<PublicController> _logger;
 
         // The Web API will only accept tokens 1) for users, and 2) having the "access_as_user" scope for this API
         // static readonly string[] scopeRequiredByApi = new string[] { "profile" };
 
-        public ImageController(ILogger<ImageController> logger)
+        public PublicController(ILogger<PublicController> logger)
         {
             _logger = logger;
         }
@@ -30,7 +30,7 @@ namespace ImageStoreApi.Controllers
         {
             // HttpContext.VerifyUserHasAnyAcceptedScope(scopeRequiredByApi);
 
-            return Content("This is a placeholder response for ImageController");
+            return Content("This is a placeholder response for PublicController");
 
             // Add code for Get
 
@@ -43,5 +43,6 @@ namespace ImageStoreApi.Controllers
             // })
             // .ToArray();
         }
+
     }
 }
