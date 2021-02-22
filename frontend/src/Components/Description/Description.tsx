acb0c76e14b1c6ae002b7750c0a106716514570b
bhattirahat <bhattir@hotmail.co.uk>
2021-02-22 16:55:18 -0500

    issue-#32 added scroll bar in description

--- frontend/src/Components/Description/Description.tsx
+++ frontend/src/Components/Description/Description.tsx
@@ -21,7 +21,7 @@ class Description extends React.Component<any, any>{
 
     render() {
         return(
-            <div>
+            <div className="overflow">
                 <IconButton id="like" onClick={() => this.setState({clicked : this.state.clicked ? false : true})} aria-label="heart">
             {this.state.clicked ? <FavoriteIcon color ="secondary" fontSize="large"/> : 
                                 <FavoriteBorderIcon color ="secondary" fontSize="large" /> }      
@@ -33,8 +33,10 @@ class Description extends React.Component<any, any>{
                
                 <p id= "inline"> <strong>Name of person</strong> </p>
                 <br/>
+               
                 <p id= "inline" className="description">This is where the description of the user will go into.</p>
-             
+
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, quaerat quasi. Dicta vel molestias repellat rem. Ea quas illum impedit libero provident architecto sapiente nam aliquam, obcaecati, cumque reprehenderit nulla?
             </div>
 
         )
