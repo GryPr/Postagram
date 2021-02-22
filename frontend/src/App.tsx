71fe3652d5fcd7d45bc31a8d882ad12e19186936
Kevin Prom <promkev@gmail.com>
2021-02-22 15:54:40 -0500

    Create image upload UI mockup #44

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import AuthenticationButton from './Components/authenticationButton';
+import React from "react";
+import "./App.css";
+import ImageUpload from "./Components/imageUpload";
 
 function App() {
   return (
-    
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <AuthenticationButton></AuthenticationButton>
-          Learn React
-        </a>
+        <ImageUpload />
       </header>
     </div>
   );
