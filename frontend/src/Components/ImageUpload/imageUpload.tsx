82bb203cb7096254595fa709b32ca0aab4587e5c
Kevin Prom <promkev@gmail.com>
2021-02-23 12:37:24 -0500

    Completed image posting

--- frontend/src/Components/ImageUpload/imageUpload.tsx
+++ frontend/src/Components/ImageUpload/imageUpload.tsx
@@ -1,17 +1,78 @@
 import { Button, TextField, Paper } from "@material-ui/core";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import "./imageUpload.css";
+import { useHistory } from "react-router-dom";
+import { useEffect } from "react";
+import { useAccount, useMsal } from "@azure/msal-react";
+import { loginRequest } from "../../Constants/authConfig";
+import {
+  InteractionRequiredAuthError,
+  SilentRequest,
+} from "@azure/msal-browser";
 
 export default function ImageUpload() {
   const [file, setFile] = useState<File>();
+  const [description, setDescription] = useState('');
+  const { instance, accounts } = useMsal();
+  const account = useAccount(accounts[0] || {})!;
+  const history = useHistory();
+
+  async function getAccessToken() {
+    const silentRequest: SilentRequest = {
+      account: account,
+      ...loginRequest,
+    };
+    try {
+      const resp = await instance
+        .acquireTokenSilent(silentRequest);
+      if (resp.accessToken) {
+        return resp.accessToken;
+      }
+    } catch (error) {
+      if (error instanceof InteractionRequiredAuthError) {
+        // fallback to interaction when silent call fails
+        instance.acquireTokenPopup(silentRequest).then((response) => {
+          return response.accessToken
+        });
+      }
+    }
+  }
+
+  async function sendData() {
+    const token = await getAccessToken();
+    console.log(token);
+
+    var formData = new FormData();
+
+    formData.append("ImageDescription", description)
+    formData.append("ImageContent", file!)
+
+    fetch("https://localhost:5001/image", {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        'Authorization': 'Bearer ' + token,
+      },
+      body: formData,
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response);
+      });
+    setTimeout(() => {
+      history.push('/')
+    }, 1000);
+  }
+
   return (
     <Paper id="bg" elevation={3}>
       <div>
         {file != null ? (
           <img id="img" src={URL.createObjectURL(file)} alt="" />
         ) : (
-          "No Image Uploaded"
-        )}
+            "No Image Uploaded"
+          )}
       </div>
       <Button variant="contained" component="label">
         Select Image
@@ -33,8 +94,9 @@ export default function ImageUpload() {
           shrink: true,
         }}
         variant="filled"
+        onChange={(event) => { setDescription(event.target.value) }}
       />
-      <Button variant="contained" component="label">
+      <Button variant="contained" component="label" onClick={sendData}>
         Submit
       </Button>
     </Paper>
